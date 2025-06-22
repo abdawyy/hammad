e7c5f7f69c4549d0df8ade7ca98cce2b9c055dd7
abdawyy <eyadomar.ok@gmail.com>
2025-06-23 02:36:00 +0300

    admin_index

---
+++ resources/lang/en/datatable.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'columns' => [
+        'id' => 'Id',
+        'name' => 'Name',
+        'email' => 'Email',
+        'role' => [
+            'name' => 'Role Name',
+        ],
+        'actions' => 'Actions'
+    ]
+];
